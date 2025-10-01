64fc41466523ed42c6e19ab81e91ca33bac54b87
aljary12 <ryadisp@gmail.com>
2025-10-02 01:37:56 +0800

    feat: use date-fns for time

--- src/utils/time.ts
+++
@@ -1,4 +0,0 @@
-export function hhmmss(date = new Date()): string {
-  const pad = (n: number) => String(n).padStart(2, '0')
-  return `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`
-}
