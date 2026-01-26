569e8bbc8b194be80fc0d1b6a9e4a14991cdbf1a
Nudin <ahmadazhar10@gmail.com>
2026-01-26 21:28:02 +0800

    Update run to satisfy action checklist

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 # Execution steps that run the CLI application
-./feedme-cli
+(
+echo "add order"
+sleep 2
+echo "add order"
+sleep 3
+echo "add vip order"
+sleep 1
+echo "add order"
+sleep 4
+echo "add bot"
+sleep 1
+echo "add bot"
+sleep 5
+echo "remove bot"
+sleep 3
+echo "add vip order"
+sleep 60
+echo "exit"
+) | ./feedme-cli
