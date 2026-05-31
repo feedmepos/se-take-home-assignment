c764656a46663be2b9c5b55eb8791b09bf8b62ae
aliceseas123-spec <aliceseas123@gmail.com>
2026-05-31 10:59:45 +0800

    fix css import path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app.css";
+import "./App.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
