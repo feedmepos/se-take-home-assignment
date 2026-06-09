51f1c07376be2fc756a496bd3a2e3647a24b7aaf
zhouyu <yu@192.168.0.100>
2026-06-09 17:21:12 +0800

    部署docs文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: path.resolve(__dirname, "src/index.tsx"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "bundle.[contenthash].js",
     publicPath: "/",
   },
