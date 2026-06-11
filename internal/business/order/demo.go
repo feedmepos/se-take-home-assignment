8749aef02a95ec43d1fc48bf10929155689f9a9e
zf766045962 <766045962@qq.com>
2026-06-11 11:31:26 +0800

    feat: built-in demo scenario

---
+++ internal/business/order/demo.go
@@ -0,0 +1,53 @@
+package order
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+func RunDemo(w io.Writer) {
+	r := NewRecorder(w)
+	demoDuration := 2 * time.Second
+	c := NewController(WithDuration(demoDuration))
+
+	fmt.Fprintln(w, "=== McDonald's Order Controller Demo ===")
+	fmt.Fprintln(w, "")
+
+	done := make(chan struct{})
+	go func() {
+		tk := time.NewTicker(100 * time.Millisecond)
+		defer tk.Stop()
+		for {
+			select {
+			case <-tk.C:
+				c.ProcessCompleted()
+			case <-done:
+				c.ProcessCompleted()
+				return
+			}
+		}
+	}()
+
+	c.NewOrder(OrderNormal)
+	c.NewOrder(OrderNormal)
+	c.NewOrder(OrderVIP)
+	c.NewOrder(OrderVIP)
+
+	time.Sleep(200 * time.Millisecond)
+	c.AddBot()
+	c.AddBot()
+	c.AddBot()
+
+	time.Sleep(100 * time.Millisecond)
+	c.RemoveBot()
+
+	time.Sleep(demoDuration + 500*time.Millisecond)
+	c.NewOrder(OrderNormal)
+
+	time.Sleep(demoDuration + 500*time.Millisecond)
+	close(done)
+
+	r.WriteSummary()
+	fmt.Fprintln(w, "=== Demo Complete ===")
+}
