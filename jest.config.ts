90afe42467d8666e8132ef3076eeb99985e55a6e
kashi93 <75873210+kashi93@users.noreply.github.com>
2026-06-19 22:00:24 +0800

    feat: initialize TypeScript project with basic setup

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import type { Config } from 'jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  },
+  testMatch: ["<rootDir>/test/**/*.test.ts"],
+};
+
+export default config;
---
+++ test/index.test.ts
@@ -0,0 +1,3 @@
+test("sanity check", () => {
+  expect(1 + 1).toBe(2);
+});
